9a69e09b9045195a9148fbd3d783bfa4dfd9a764
Zhafir18 <m.zhafirghiffari@gmail.com>
2024-10-14 14:35:12 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'project',
+    component: ProjectComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
