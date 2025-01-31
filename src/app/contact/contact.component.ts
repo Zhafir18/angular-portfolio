a3a30478d5ad5855ce11082997ddceb7381f4228
ZhafirGhiffari <muhammad.ghiffari@indocyber.id>
2025-01-31 09:58:59 +0700

    update terbaru

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css',
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
   darkTheme: boolean = false;
+  contactForm: FormGroup;
 
-  contact = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: [''],
+      email: ['', [Validators.email]],
+      message: [''],
+    });
+  }
 
   sendEmail() {
-    const mailtoLink = `mailto:${
-      this.contact.email
-    }?subject=Contact%20Form%20Submission&body=Name:%20${encodeURIComponent(
-      this.contact.name
-    )}%0D%0AEmail:%20${encodeURIComponent(
-      this.contact.email
-    )}%0D%0AMessage:%20${encodeURIComponent(this.contact.message)}`;
+    if (this.contactForm.valid) {
+      const { name, email, message } = this.contactForm.value;
+      const mailtoLink = `mailto:?subject=Contact%20Form%20Submission&body=Name:%20${encodeURIComponent(
+        name
+      )}%0D%0AEmail:%20${encodeURIComponent(
+        email
+      )}%0D%0AMessage:%20${encodeURIComponent(message)}`;
 
-    window.location.href = mailtoLink;
+      window.location.href = mailtoLink;
+    } else {
+      alert('Please fill out all fields correctly.');
+    }
   }
 }
